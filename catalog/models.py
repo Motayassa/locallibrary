0e090fe21cf26b9d2e32ce967bd8615a1b9af83b
Motayassa <malovilyajob@gmail.com>
2024-01-09 17:00:59 +0300

    Add model for author

--- catalog/models.py
+++ catalog/models.py
@@ -23,7 +23,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
-    isbn = models.CharField('ISBN',max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
 
     def __str__(self):
@@ -86,4 +86,4 @@ class Author(models.Model):
         """
         String for representing the Model object.
         """
-        return '{0}, {1})'.format(self.last_name, self.first_name)
+        return '%s, %s' % (self.last_name, self.first_name)
