7d017e864a09b069ce57d318f16ef9ebd84611cf
Motayassa <malovilyajob@gmail.com>
2024-01-09 16:48:43 +0300

    Add model for author

--- catalog/models.py
+++ catalog/models.py
@@ -65,3 +65,25 @@ class BookInstance(models.Model):
         String for representing the Model object
         """
         return '{0} ({1})'.format(self.id, self.book.title)
+
+
+class Author(models.Model):
+    """
+    Model representing an author.
+    """
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular author instance.
+        """
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return '{0}, {1})'.format(self.last_name, self.first_name)
