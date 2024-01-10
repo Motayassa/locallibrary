96496f283a50120bbafe444973f3266f5954938e
Motayassa <malovilyajob@gmail.com>
2024-01-10 20:49:15 +0300

    Update view for AdminModel

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,7 +6,8 @@ from .models import Author, Genre, Book, BookInstance, Language
 
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('last_name', 'first_name',
+                    'date_of_birth', 'date_of_death')
 
 
 admin.site.register(Author, AuthorAdmin)
