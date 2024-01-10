d7dca83781c0b29ccc6db6b40789eea3b76147b2
Motayassa <malovilyajob@gmail.com>
2024-01-10 20:38:01 +0300

    Fix a model registration of book

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,9 +2,17 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Author, Genre, Book, BookInstance, Language
+
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book)
-admin.site.register(Author)
+# admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(BookInstance)
 admin.site.register(Language)
-
