a770cc30f02c3b1da97687d7c9e39913890d3612
Motayassa <malovilyajob@gmail.com>
2024-01-11 04:28:21 +0300

    Upgrade admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -17,9 +17,15 @@ admin.site.register(Author, AuthorAdmin)
 # Register the Admin classes for Book using the decorator
 
 
+class BooksInstanceInline(admin.TabularInline):
+    extra = 0
+    model = BookInstance
+
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
 
 
 # Register the Admin classes for BookInstance using the decorator
