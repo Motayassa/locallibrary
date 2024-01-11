f7e0cd88c1c734fbdfcd9b03d6089fb846e61a5f
Motayassa <malovilyajob@gmail.com>
2024-01-11 04:20:47 +0300

    Add sections in admin panel for bookinstance

--- catalog/admin.py
+++ catalog/admin.py
@@ -8,6 +8,8 @@ from .models import Author, Genre, Book, BookInstance, Language
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name',
                     'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name',
+              ('date_of_birth', 'date_of_death')]
 
 
 admin.site.register(Author, AuthorAdmin)
@@ -25,6 +27,14 @@ class BookAdmin(admin.ModelAdmin):
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
 
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
 # Register the admin class with the associated model
 # admin.site.register(Book)
