f4d7742f8b3149d603c2e07a8fff0bc71121bddf
Motayassa <malovilyajob@gmail.com>
2024-01-14 19:55:03 +0300

    Add url for author-list

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     re_path(r'^books/$', views.BookListView.as_view(), name='books'),
     re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(),
             name='book-detail'),
+    re_path(r'^authors/$', views.AuthorsListView.as_view(), name='authors'),
 ]
