c9a1a94a772128632f4a229025406e3e2fd5d990
Motayassa <malovilyajob@gmail.com>
2024-01-14 19:07:04 +0300

    Fix bug

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path
+from django.urls import re_path
 from . import views
-from django.conf.urls import url
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^books/$', views.BookListView.as_view(), name='books'),
-    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(),
-        name='book-detail'),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(),
+            name='book-detail'),
 ]
