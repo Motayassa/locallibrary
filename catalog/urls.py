60fbce21b304b47f3ae985e0b84dd451cf204a32
Motayassa <malovilyajob@gmail.com>
2024-01-14 20:00:55 +0300

    Fix name for views

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     re_path(r'^books/$', views.BookListView.as_view(), name='books'),
     re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(),
             name='book-detail'),
-    re_path(r'^authors/$', views.AuthorsListView.as_view(), name='authors'),
+    re_path(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
 ]
